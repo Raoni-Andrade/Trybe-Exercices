c28b4cc3b1e42bbae10f42ea91655818121389c5
Raoni Andrade <raonio.andrade@gmail.com>
2022-07-21 19:10:29 -0300

    Exercício 5 feito

--- 1 - Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/1 - Introdução e Primeiros passos/script.js
+++ 1 - Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/1 - Introdução e Primeiros passos/script.js
@@ -1,21 +1,39 @@
+// Exercício 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-console.log(numbers);
-let soma = 0;
 
+let maiorNumero = 0;
+
+// Exercício 5
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index]
+    }
+}
+
+console.log(maiorNumero);
+
+
+
+
+
+// Exercício 2
 // for (let index = 0; index < numbers.length; index += 1) {
 //     console.log(numbers[index])
 // }
 
-for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
-}
+// Exercício 3
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
 
-console.log(soma / numbers.length);
+// console.log(soma / numbers.length);
 
-if (soma / numbers.length > 20) {
-    console.log("Valor maior que 20");
-}
+// Exercício 4
+// if (soma / numbers.length > 20) {
+//     console.log("Valor maior que 20");
+// }
 
-else {
-    console.log("Valor menor que 20");
-}
+// else {
+//     console.log("Valor menor que 20");
+// }
+

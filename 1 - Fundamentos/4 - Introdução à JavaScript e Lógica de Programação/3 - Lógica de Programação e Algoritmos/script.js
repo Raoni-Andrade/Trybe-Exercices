b6a024d1be8f9798d1e676627173c7b3416c9fc3
Raoni Andrade <raonio.andrade@gmail.com>
2022-07-22 16:23:48 -0300

    For/in e for/of em JavaScript

--- 1 - Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/3 - Lógica de Programação e Algoritmos/script.js
+++ 1 - Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/3 - Lógica de Programação e Algoritmos/script.js
@@ -1,16 +1,54 @@
-let player = {
-    name: "Marta",
-    lastName: "Silva",
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3
-    },
-    bestInWorld: [2006, 2007, 2008, 2009, 2010, 2018]
-};
+// let player = {
+//     name: "Marta",
+//     lastName: "Silva",
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3
+//     },
+//     bestInWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// };
 
-console.log("A jogadora " + player.name + " " + player.lastName + " " + "tem " + player.age + " " + "anos de idade.");
+// console.log("A jogadora " + player.name + " " + player.lastName + " " + "tem " + player.age + " " + "anos de idade.");
 
-console.log("A jogadora " + player.name + " " + player.lastName + " " + "foi eleita a melhor do mundo 6 vezes. " + player.bestInWorld + ".")
+// console.log("A jogadora " + player.name + " " + player.lastName + " " + "foi eleita a melhor do mundo 6 vezes. " + player.bestInWorld + ".")
 
-console.log("A jogadora " + player.name + " " + player.lastName + " " + "possui " + player.medals.golden + " " + "medalhas de ouro e " + player.medals.silver + " " + "medalhas de prata.")
+// console.log("A jogadora " + player.name + " " + player.lastName + " " + "possui " + player.medals.golden + " " + "medalhas de ouro e " + player.medals.silver + " " + "medalhas de prata.")
+
+
+// let cars = ['Saab', 'Volvo', 'BMW', 'Peugeot'];
+
+// for (let index in cars) {
+//   console.log(index, cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+
+//   let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for (let index in names) {
+//     console.log("Olá, " + names[index] + "!")
+//   }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let index in car) {
+      console.log(index, car[index])
+  }

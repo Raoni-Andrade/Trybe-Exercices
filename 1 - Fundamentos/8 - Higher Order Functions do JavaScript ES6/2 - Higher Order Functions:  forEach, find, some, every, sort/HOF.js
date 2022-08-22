1a3a3e934d1b4f03b6dd7de76061738e6cc5daf3
Raoni Andrade <raonio.andrade@gmail.com>
2022-08-22 16:15:33 -0300

    Mais exercícios feitos

--- 1 - Fundamentos/8 - Higher Order Functions do JavaScript ES6/2 - Higher Order Functions:  forEach, find, some, every, sort/HOF.js
+++ 1 - Fundamentos/8 - Higher Order Functions do JavaScript ES6/2 - Higher Order Functions:  forEach, find, some, every, sort/HOF.js
@@ -1,3 +1,4 @@
+// Exercício 1 Array.forEach
 // const emailListInData = [
 //   'roberta@email.com',
 //   'paulo@email.com',
@@ -11,10 +12,34 @@
 
 // emailListInData.forEach(showEmailList);
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+// Exercício 1 Array.find
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);  
+// const findDivisibleBy3And5 = () => {
+//   return numbers.find((number) => number % 3 === 0 && number % 5 === 0);  
+// }
+
+// console.log(findDivisibleBy3And5());
+
+// Exercício 2 Array.find
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = () => {
+//   return names.find((name) => name.length === 5);
+// }
+
+// console.log(findNameWithFiveLetters());
+
+// Exercício 3 Array.find
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id);
+  // Adicione seu código aqui
 }
 
-console.log(findDivisibleBy3And5());
+console.log(findMusic('31031685'))
